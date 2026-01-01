4d03ac21080fb51155713bb216c4be767e23bc91
Dmitriy <rhyzome88@gmail.com>
2026-01-01 17:18:11 +0300

    Refactor on functional components Main and Search

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,38 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Preloader } from '../components/Preloader'
 import { Movies } from '../components/Movies'
 import { Search } from '../components/Search'
 
-class Main extends React.Component {
-	state = {
-		movies: [],
-		isLoading: false,
-		error: null,
+function Main() {
+	const [movies, setMovies] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(null);
+
+	const updateMovies = (newMoviesData, newIsLoading) => {
+		setMovies(newMoviesData);
+		setIsLoading(newIsLoading);
+	};
+	
+	const updateErr = (newIsLoading, newErrData) => {
+		setError(newErrData);
+		setIsLoading(newIsLoading);
 	};
 
-	updateMovies = (newMoviesData, newIsLoading) => {
-		this.setState({ movies: newMoviesData, isLoading: newIsLoading });
-	};
-
-	updateErr = (newIsLoading, newErrData) => {
-		this.setState({ isLoading: newIsLoading, error: newErrData });
-	};
-
-	render() {
-		const { movies } = this.state;
-		return (
-			<main className='container content'>
-				<>
-					<Search
-						movies={movies}
-						onUpdateMovies={this.updateMovies}
-						onUpdateErr={this.updateErr}
-					/>
-					<Movies movies={movies} />
-				</>
-			</main>
-		);
-	}
+	return (
+		<main className='container content'>
+			<>
+				<Search
+					movies={movies}
+					onUpdateMovies={updateMovies}
+					onUpdateErr={updateErr}
+				/>
+				<Movies movies={movies} />
+			</>
+		</main>
+	)
 }
 
 export { Main }
